6126efbba620f28a1fdf2b19dd471597327138b1
Seth Teichman <tei06398@stu.niskyschools.org>
2017-11-13 22:06:46 -0500

    Created RRAuton Current functionality: Essentially, just a rewrite of the VuforiaAuton OpMode, with less verbose comments, and a tad bit more redundancy in readings. Will test for functionality at Friday's Meeting.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAuton.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.*;
+
+/**
+ * Created 11/13/2017
+ */
+@Autonomous(name = "Relic Recovery Official Auton Mode")
+public class RRAuton extends LinearOpMode {
+
+    VuforiaLocalizer vuforia;
+
+    char readVuMark(VuforiaTrackable relicTemplate) {
+        RelicRecoveryVuMark vuMark = null;
+        int total = 0;
+        int left = 0;
+        int right = 0;
+        int center = 0;
+        while (total < 3) {
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            if (vuMark == RelicRecoveryVuMark.LEFT) {
+                left++;
+                total++;
+            } else if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                right++;
+                total++;
+            } else if (vuMark == RelicRecoveryVuMark.CENTER) {
+                center++;
+                total++;
+            }
+        }
+        if (left > right && left > center) {
+            return 'l'; //Left is most likely Correct
+        } else if (right > left && right > center) {
+            return 'r'; //Right is most likely Correct
+        } else if (center > right && center > left) {
+            return 'c'; //Center is most likely Correct
+        }
+        return '!';
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Tell Vuforia to display video feed
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        //Disable Video Feed with the following:
+        //VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+
+        //Set License Key, tell Vuforia to use back Camera, finalize Vuforia Parameters for Localizer
+        parameters.vuforiaLicenseKey = "AUrG/9b/////AAAAGYb5udEIt0W7p8AYwNbs5lUO4Ojghb2IvJN64Q6ZnRvUfbl59Au5c48n2lykKrsfZgYx9m2HpOgdNFmLaxhilDQIc0mmohbk5IjXvKkGGJR4OiqNtqYVDncXZpb/esaPeFTLtkbJFAlEs+oPwcAKoO5FctEuFyEgz1IJc6/MRphweDiXuJ86Rqs81UVeOlNXdr3QtZazJcViHHeWkv5pJUvefJWbjXSZvOZFlITDaTbyPIibQsHsbDg+B0IBMHK+d8fs58anmJ1VJEoQ9Xo0YDpHgosgx996zfSjhAK8YcIxLQdCkFbJtQtkpAl14jz+Yz86QvP8AMtFeMRuIYrPWhQvPJ3VU/Jm1Qmz+p/jbJ8k";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        //Load in the VuMark dataset from assets
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
+        //Wait for OpMode Init Button to be Pressed
+        waitForStart();
+
+        //Activate the VuMark Dataset as Current Tracked Object
+        relicTrackables.activate();
+
+        //Get a semi-reliable reading of the Pictograph
+        int total = 0;
+        char pictograph = 'E';
+        while (total<3) {
+            pictograph = readVuMark(relicTemplate);
+            if (pictograph != '!') {
+                total = 3;
+            } else {
+                total++;
+            }
+        }
+        if (pictograph == '!') {
+            telemetry.addData("Pictograph", "Unreliable");
+            //Displays in the event that 3/3 times, the data returned by readVuMark() has been 1L,1C,1R, not allowing for a logical interpretation.
+        } else if (pictograph == 'l') {
+            telemetry.addData("Pictograph", "Left");
+        } else if (pictograph == 'r') {
+            telemetry.addData("Pictograph", "Right");
+        } else if (pictograph == 'c') {
+            telemetry.addData("Pictograph", "Center");
+        } else {
+            telemetry.addData("Pictograph", "ERROR");
+            //Displays only if the initial value of pictograph remains unchanged, which shouldn't occur.
+        }
+        telemetry.update();
+
+        //TODO: For Friday, (or maybe earlier,) begin work on either Color Detection for the Jewels or Motion for Placing Cube in CryptoBox
+    }
+}
