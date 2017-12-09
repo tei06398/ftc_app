10424c98077895a6eca9b43c37f816d5078a7bff
sjhuang26 <sjhuang26@gmail.com>
2017-12-09 16:38:14 -0500

    Added left trigger to go fast
    This is important when getting on the balancing stone.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTeleOp.java
@@ -7,15 +7,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import static org.firstinspires.ftc.teamcode.RobotDriving.MAX_SPEED_RATIO;
 import static org.firstinspires.ftc.teamcode.RobotDriving.MIN_SPEED_RATIO;
+import static org.firstinspires.ftc.teamcode.RobotDriving.NORMAL_SPEED_RATIO;
 
-/**
- * Last Modified 11/7/2017
- */
 @TeleOp(name = "Relic Recovery Official Tele-Op Mode")
 public class RRTeleOp extends OpMode {
-    /* Declare here any fields you might find useful. */
     protected DcMotor motorLF = null;
-    // Declares motors
     protected DcMotor motorRF = null;
     protected DcMotor motorLB = null;
     protected DcMotor motorRB = null;
@@ -36,10 +32,8 @@ public class RRTeleOp extends OpMode {
 
 
     private static double BLOCK_ROTATION_WEIGHT = 0.5; // make this final later
-
+    
     public void loop() {
-        steering.setSpeedRatio((this.gamepad1.right_trigger > 0.5) ? MIN_SPEED_RATIO : MAX_SPEED_RATIO);
-
         // TESTING
         if (this.gamepad1.a) BLOCK_ROTATION_WEIGHT += 0.05;
         if (this.gamepad1.b) BLOCK_ROTATION_WEIGHT -= 0.05;
@@ -65,18 +59,28 @@ public class RRTeleOp extends OpMode {
 
         // Arrow keys: also driving
         if (this.gamepad1.dpad_right) {
-            steering.moveDegrees(0,.3);
+            steering.moveDegrees(0);
         }
         if (this.gamepad1.dpad_up) {
-            steering.moveDegrees(90,.3);
+            steering.moveDegrees(90);
         }
         if (this.gamepad1.dpad_left) {
-            steering.moveDegrees(180,.3);
+            steering.moveDegrees(180);
         }
         if (this.gamepad1.dpad_down) {
-            steering.moveDegrees(270,.3);
+            steering.moveDegrees(270);
         }
-
+        
+        // Right trigger: minimum speed
+        if (this.gamepad1.right_trigger > 0.5) {
+            steering.setSpeedRatio(MIN_SPEED_RATIO);
+        } else if (this.gamepad1.left_trigger > 0.5) {
+            // Left trigger: maximum speed
+            steering.setSpeedRatio(MAX_SPEED_RATIO);
+        } else {
+            steering.setSpeedRatio(NORMAL_SPEED_RATIO);
+        }
+        
         // Left bumper: move around block counterclockwise
         if (this.gamepad1.left_bumper) steering.aroundPoint(false, BLOCK_ROTATION_WEIGHT);
 
@@ -136,21 +140,24 @@ public class RRTeleOp extends OpMode {
         this.motorRF = this.hardwareMap.dcMotor.get("rfMotor");
         this.motorLB = this.hardwareMap.dcMotor.get("lbMotor");
         this.motorRB = this.hardwareMap.dcMotor.get("rbMotor");
+        
         this.motorWinch = this.hardwareMap.dcMotor.get("winchMotor");
         this.motorRelicSlide = this.hardwareMap.dcMotor.get("relicSlideMotor");
         this.servoGlyphter = this.hardwareMap.servo.get("glyphterServo");
         this.servoGlyphterRotation = this.hardwareMap.servo.get("glyphterRotationServo");
+        
         this.motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         this.motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         this.motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         this.motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        
         this.ultrasonicLeft = this.hardwareMap.ultrasonicSensor.get("ultrasonicLeft");
         this.ultrasonicRight = this.hardwareMap.ultrasonicSensor.get("ultrasonicRight");
         this.ultrasonicLF = this.hardwareMap.ultrasonicSensor.get("ultrasonicLF");
         this.ultrasonicRF = this.hardwareMap.ultrasonicSensor.get("ultrasonicRF");
-
-
+        
         robotDriving = new RobotDriving(motorLF,motorLB,motorRF,motorRB,telemetry);
+        
         gunnerFunction = new GunnerFunction(motorWinch, motorRelicSlide, servoGlyphter, servoGlyphterRotation, telemetry);
         
         steering = robotDriving.getSteering();
