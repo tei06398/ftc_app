cc66ff427cbb1470b335f4d731988bcbed690b28
sjhuang26 <sjhuang26@gmail.com>
2018-01-05 18:56:36 -0500

    More teleop changes 1/5/2018

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTeleOp.java
@@ -11,36 +11,20 @@ import static org.firstinspires.ftc.teamcode.RobotDriving.NORMAL_SPEED_RATIO;
 
 @TeleOp(name = "Relic Recovery Official Tele-Op Mode")
 public class RRTeleOp extends OpMode {
-    protected DcMotor motorLF = null;
-    protected DcMotor motorRF = null;
-    protected DcMotor motorLB = null;
-    protected DcMotor motorRB = null;
-
     protected RobotDriving robotDriving;
     protected RobotDriving.Steering steering;
     protected GunnerFunction gunnerFunction;
 
-    private boolean allowGamepad2B = true;
+    private boolean allowGamepad2B = true; // Toggles gamepad2's B key
 
-    private boolean disableA1 = false;
-    private boolean disableB1 = false;
+    // The weight given to rotation (as opposed to left/right strafe) during pivoting
+    private final static double BLOCK_ROTATION_WEIGHT = 0.5;
 
+    public void init() {
+        // No motors/servos are instantiated here because everything is done in the RobotDriving, GunnerFunction, etc.
+        // classes.
 
-    private final static double BLOCK_ROTATION_WEIGHT = 0.5; // make this final later
-
-    public void init(){
-        //Instantiates motors and servos, sets operating mode
-        this.motorLF = this.hardwareMap.dcMotor.get("lfMotor");
-        this.motorRF = this.hardwareMap.dcMotor.get("rfMotor");
-        this.motorLB = this.hardwareMap.dcMotor.get("lbMotor");
-        this.motorRB = this.hardwareMap.dcMotor.get("rbMotor");
-
-        this.motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        this.motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        this.motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        this.motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        robotDriving = new RobotDriving(motorLF,motorLB,motorRF,motorRB,telemetry);
+        robotDriving = new RobotDriving(hardwareMap, telemetry);
 
         gunnerFunction = new GunnerFunction(hardwareMap, telemetry);
 
@@ -160,6 +144,7 @@ public class RRTeleOp extends OpMode {
             }
         }
 
+        // Finish the steering, which puts power in the motors.
         steering.finishSteering();
 
         telemetry.update();
