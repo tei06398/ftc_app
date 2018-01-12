b855a489b093b1d56e7481f2b8d23d5a13148444
kam07440 <gkammer601@gmail.com>
2018-01-12 16:18:13 -0500

    Push so we can pull from Development Laptop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -10,50 +10,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  */
 @TeleOp(name = "Test Mode")
 public class TestTeleOp extends OpMode {
-    /* Declare here any fields you might find useful. */
-    // Declares motors
-    protected DcMotor motorLF = null;
-    protected DcMotor motorRF = null;
-    protected DcMotor motorLB = null;
-    protected DcMotor motorRB = null;
-    protected Servo glyphterServoLeft = null;
-    protected Servo glyphterServoRight = null;
-
-    GunnerFunction gunnerFunction = new GunnerFunction(hardwareMap, telemetry);
-
-    private double position = 100;
-    private double position2 = 100;
+    private GunnerFunction gunnerFunction;
 
     public void loop(){
-        if (this.gamepad1.y) position += 0.1;
-        if (this.gamepad1.a) position -= 0.1;
-        if (this.gamepad1.x) position2 += 0.1;
-        if (this.gamepad1.b) position2 -= 0.1;
         if (this.gamepad1.left_bumper) {gunnerFunction.lowerJewelPusher();}
         if (this.gamepad1.right_bumper) {gunnerFunction.raiseJewelPusher();}
-
-        this.glyphterServoLeft.setPosition(position);
-        this.glyphterServoRight.setPosition(position2);
-
-        telemetry.addData("glyphterServoLeft", position);
-        telemetry.addData("glyphterServoRight", position2);
-        telemetry.addData("glyphterServoLeft reading", glyphterServoLeft.getPosition());
-        telemetry.addData("glyphterServoRight reading", glyphterServoRight.getPosition());
-
-        telemetry.update();
     }
 
     public void init(){
-        //Instantiates motors and servos, sets operating mode
-        this.motorLF = this.hardwareMap.dcMotor.get("lfMotor");
-        this.motorRF = this.hardwareMap.dcMotor.get("rfMotor");
-        this.motorLB = this.hardwareMap.dcMotor.get("lbMotor");
-        this.motorRB = this.hardwareMap.dcMotor.get("rbMotor");
-        this.glyphterServoLeft = this.hardwareMap.servo.get("glyphterServoLeft");
-        this.glyphterServoRight = this.hardwareMap.servo.get("glyphterServoRight");
-        this.motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        this.motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        this.motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        this.motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        gunnerFunction = new GunnerFunction(hardwareMap, telemetry);
     }
 }
