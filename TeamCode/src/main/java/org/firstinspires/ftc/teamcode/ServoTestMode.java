aec0d12cd69d1f557d271b13ba5afbf7b1a4a72b
sjhuang26 <sjhuang26@gmail.com>
2018-01-26 17:54:06 -0500

    Midmeeting commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestMode.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="Servo Test")
+public class ServoTestMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo servo = this.hardwareMap.servo.get("digitalServo");
+        double position = 0;
+        final double rate = 1.0 / 180;
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_left) {
+                position -= rate;
+            }
+            if (gamepad1.dpad_right) {
+                position += rate;
+            }
+            if (position < 0.0) {
+                position = 0;
+            }
+            if (position > 1.0) {
+                position = 1;
+            }
+            if (gamepad1.a) servo.setDirection(Servo.Direction.FORWARD);
+            if (gamepad1.b) servo.setDirection(Servo.Direction.REVERSE);
+            servo.setPosition(position);
+            telemetry.addData("Servo position", position);
+            telemetry.update();
+            sleep(10);
+        }
+    }
+}
