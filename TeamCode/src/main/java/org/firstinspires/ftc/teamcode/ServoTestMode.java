e33f17c1008aa53f579554940e3734b62cce0db5
sjhuang26 <sjhuang26@gmail.com>
2018-01-29 16:59:40 -0500

    Mid meeting commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestMode.java
@@ -2,15 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.*;
 
 @TeleOp(name="Servo Test")
 public class ServoTestMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Servo servo = this.hardwareMap.servo.get("digitalServo");
+        //Servo servo = this.hardwareMap.servo.get("digitalServo");
         DcMotor motor = this.hardwareMap.dcMotor.get("motor");
+        PWMOutput pwmOutput = hardwareMap.pwmOutput.get("digitalServo");
+        Servo servo = this.hardwareMap.servo.get("digitalServo");
+
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         double position = 0;
         final double rate = 1.0 / 180;
@@ -29,8 +31,8 @@ public class ServoTestMode extends LinearOpMode {
             if (position > 1.0) {
                 position = 1;
             }
-            if (gamepad1.a) servo.setDirection(Servo.Direction.FORWARD);
-            if (gamepad1.b) servo.setDirection(Servo.Direction.REVERSE);
+            if (gamepad1.a) pwmOutput.setPulseWidthOutputTime(1000);
+            if (gamepad2.b) pwmOutput.setPulseWidthOutputTime(2000);
             if (gamepad1.x) {
                 motor.setPower(.5);
             } else if (gamepad1.y) {
