aa381112e77dd184d61b3406dd0185adf4af9a04
Seth Teichman <tei06398@stu.niskyschools.org>
2018-02-02 20:51:34 -0500

    End of Testing for Today Extensive work planned for Tomorrow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GunnerFunction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GunnerFunction.java
@@ -16,8 +16,8 @@ public class GunnerFunction {
     private final DcMotor motorRelicSlide;
     private final DcMotor motorWinch;
     private final Servo servoJewelPusher;
-    private final TwoStateServo servoGlyphterLeft;
-    private final TwoStateServo servoGlyphterRight;
+    private final TwoStateServo servoGlyphterLeft = null;
+    private final TwoStateServo servoGlyphterRight = null;
     private final TwoStateServo relicGrabber;
     private final TwoStateServo relicLifter;
     private final Telemetry telemetry;
@@ -26,11 +26,11 @@ public class GunnerFunction {
         // Load the needed devices from the hardware map
         this.motorWinch = hardwareMap.dcMotor.get("winchMotor");
         this.motorRelicSlide = hardwareMap.dcMotor.get("relicSlideMotor");
-        this.servoGlyphterLeft = new TwoStateServo(hardwareMap.servo.get("glyphterServoLeft"), 0, 0.5, 1, true);
-        this.servoGlyphterRight = new TwoStateServo(hardwareMap.servo.get("glyphterServoRight"), 0, 0.5, 1, true);
+        //this.servoGlyphterLeft = new TwoStateServo(hardwareMap.servo.get("glyphterServoLeft"), 0, 0.5, 1, true);
+        //this.servoGlyphterRight = new TwoStateServo(hardwareMap.servo.get("glyphterServoRight"), 0, 0.5, 1, true);
         this.servoJewelPusher = hardwareMap.servo.get("jewelPusher");
         this.relicGrabber = new TwoStateServo(hardwareMap.servo.get("relicGrabber"), 0, 0.5);
-        this.relicLifter = new TwoStateServo(hardwareMap.servo.get("relicLifter"), 0, 0.5);
+        this.relicLifter = new TwoStateServo(hardwareMap.servo.get("relicLifter"), 0, 0.9);
 
         this.telemetry = telemetry;
 
@@ -39,65 +39,78 @@ public class GunnerFunction {
 
     public void upWinch() {
         motorWinch.setPower(0.75);
+        telemetry.log().add("Up winch");
     }
 
     public void downWinch() {
         motorWinch.setPower(-0.75);
+        telemetry.log().add("Down winch");
     }
 
     public void stopWinch() {
         motorWinch.setPower(0);
+        telemetry.log().add("Stop winch");
     }
 
     public void openGlyphter() {
         servoGlyphterLeft.passive();
         servoGlyphterRight.passive();
+        telemetry.log().add("Glyphter open");
     }
 
     public void closeGlyphter() {
         servoGlyphterLeft.active();
         servoGlyphterRight.active();
+        telemetry.log().add("Glyphter close");
     }
 
     public void openGlyphterIncremental() {
         servoGlyphterLeft.incrementTowardsPassive();
         servoGlyphterRight.incrementTowardsPassive();
+        telemetry.log().add("Glyphter open incremental");
     }
 
     public void closeGlyphterIncremental() {
         servoGlyphterLeft.incrementTowardsActive();
         servoGlyphterRight.incrementTowardsActive();
+        telemetry.log().add("Glyphter close incremental");
     }
 
-    public void lowerJewelPusher() { servoJewelPusher.setPosition(JEWELPUSHER_SERVO_DOWN_POS); }
+    public void lowerJewelPusher() { servoJewelPusher.setPosition(JEWELPUSHER_SERVO_DOWN_POS);telemetry.log().add("Lower jewel pusher"); }
 
-    public void raiseJewelPusher() { servoJewelPusher.setPosition(JEWELPUSHER_SERVO_UP_POS); }
+    public void raiseJewelPusher() { servoJewelPusher.setPosition(JEWELPUSHER_SERVO_UP_POS);telemetry.log().add("Raise jewel pusher"); }
 
     public void expandRelicSlide() {
         // TODO: maybe make bigger
-        motorRelicSlide.setPower(0.1);
+        motorRelicSlide.setPower(0.5);
+        telemetry.log().add("Expand relic slide");
     }
 
     public void retractRelicSlide() {
-        motorRelicSlide.setPower(-0.1);
+        motorRelicSlide.setPower(-0.5);
+        telemetry.log().add("Retract relic slide");
     }
 
     public void stopRelicSlide() {
         motorRelicSlide.setPower(0);
+        telemetry.log().add("Stop relic slide");
     }
 
     public void toggleRelicGrabber() {
         relicGrabber.toggle();
+        telemetry.log().add("Toggle relic grabber");
     }
 
     public void toggleRelicGrabberPivot() {
         relicLifter.toggle();
+        telemetry.log().add("Toggle relic grabber pivot");
     }
 
     public void reset() {
-        closeGlyphter();
+        //closeGlyphter();
         relicGrabber.passive();
         relicLifter.passive();
         servoJewelPusher.setPosition(JEWELPUSHER_SERVO_UP_POS);
+        telemetry.log().add("Reset");
     }
 }
